a51dc11fbea3026fbce5bd4e21a089aad773e010
CorentinTh <corentin.thomasset74@gmail.com>
2020-01-22 17:32:34 +0100

    refactor: clean lint

--- src/Services/spotlight.js
+++ src/Services/spotlight.js
@@ -1,36 +1,36 @@
-import axios from 'axios'
+import axios from 'axios';
 
 function makeRequestSpotlight (confidence = 0.5, query) {
   return new Promise((resolve, reject) => {
-    query = encodeURIComponent(query)
-    let URL = 'https://api.dbpedia-spotlight.org/en/annotate?text=' + query + '&confidence=' + confidence
+    query = encodeURIComponent(query);
+    let URL = 'https://api.dbpedia-spotlight.org/en/annotate?text=' + query + '&confidence=' + confidence;
     axios.get(URL)
       // fetch(URL, options)
       .then((result) => {
         if (result.status < 300 && result.status >= 200) {
-          return result.data
+          return result.data;
         } else {
-          return { 'Resources': [] }
+          return { 'Resources': [] };
         }
       })
       .then((data) => {
         if (data['Resources']) {
-          let URIs = []
+          let URIs = [];
           data['Resources'].forEach((object) => {
-            URIs.push(object['@URI'])
-          })
-          resolve(URIs)
+            URIs.push(object['@URI']);
+          });
+          resolve(URIs);
         } else {
-          resolve([])
+          resolve([]);
         }
       })
       .catch(error => {
-        console.log(error.response.status + ' ' + error.response.statusMessage)
-        resolve([])
-      })
-  })
+        console.log(error.response.status + ' ' + error.response.statusMessage);
+        resolve([]);
+      });
+  });
 }
 
 export {
   makeRequestSpotlight
-}
+};

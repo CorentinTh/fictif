f2760a25e1de4f1db9b82d61b082315252041a85
CorentinTh <corentin.thomasset74@gmail.com>
2020-01-22 17:49:39 +0100

    refactor: moved spotlight.js from 'Services' to 'services'

--- test/services/spotlight.test.js
+++ test/services/spotlight.test.js
@@ -1,7 +1,7 @@
-import { makeRequestSpotlight } from '../../src/Services/spotlight'
+import { makeRequestSpotlight } from '../../src/services/spotlight';
 
 it('should return error 400 if empty params', async () => {
-  const result = await makeRequestSpotlight(0.8)
+  const result = await makeRequestSpotlight(0.8);
   /*
   expect.assertions(1)
   await expect(makeRequestSpotlight()).rejects.toMatchObject(
@@ -11,26 +11,26 @@ it('should return error 400 if empty params', async () => {
     }
   )
   */
-  expect(result).toEqual([])
-})
+  expect(result).toEqual([]);
+});
 
 it('should call spotlight api', async () => {
   const query = 'Mr. Bean is a British sitcom created by Rowan Atkinson and Richard Curtis, produced by Tiger Aspect ' +
     'and starring Atkinson as the title character. The sitcom consisted of 15 episodes that were co-written by ' +
     'Atkinson alongside Curtis and Robin Driscoll; for the pilot, it was co-written by Ben Elton. The series was ' +
     'originally broadcast on ITV, beginning with the pilot on 1 January 1990[1] and ending with The Best Bits of ' +
-    'Mr. Bean 15 December 1995.'
+    'Mr. Bean 15 December 1995.';
 
-  const result = await makeRequestSpotlight(0.8, query)
+  const result = await makeRequestSpotlight(0.8, query);
 
-  console.log(result)
-  expect(result).not.toHaveLength(0)
-})
+  console.log(result);
+  expect(result).not.toHaveLength(0);
+});
 
 it('should call spotlight api', async () => {
-  const query = 'pzkapdazd'
-  const result = await makeRequestSpotlight(0.8, query)
+  const query = 'pzkapdazd';
+  const result = await makeRequestSpotlight(0.8, query);
 
-  console.log(result)
-  expect(result).toEqual([])
-})
+  console.log(result);
+  expect(result).toEqual([]);
+});

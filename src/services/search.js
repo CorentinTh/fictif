7a9da03bee345e78fb5cc951050c640d2224e5f0
CorentinTh <corentin.thomasset74@gmail.com>
2020-01-27 16:36:08 +0100

    feat: better results

--- src/services/search.js
+++ src/services/search.js
@@ -3,15 +3,17 @@ import { sparqlExecutor } from './sparql';
 const cleanQuery = query => query.toLowerCase().replace(/(["'\\])/g, '\\$1').replace(/\s\s+/g, ' ').trim().split(' ');
 
 const buildRequest = keywords => {
-  const varAbstract = keywords.map((_, i) => `?ab${i}`);
+  const varAbst = keywords.map((_, i) => `?ab${i}`);
   const varName = keywords.map((_, i) => `?na${i}`);
+  const varDesc = keywords.map((_, i) => `?de${i}`);
 
   return `
+
     select distinct
       ?name
       ?thumbnail
       ?description
-      ((${varAbstract.join('+')}) + (${varName.join('+')}) as ?score)
+      ((${varAbst.join('+')}) + (${varName.join('*3 +')}*3) + (${varDesc.join('*2 +')}*2) as ?score)
     where {
 
       ?subject rdf:type <http://dbpedia.org/ontology/FictionalCharacter> .
@@ -20,10 +22,11 @@ const buildRequest = keywords => {
       OPTIONAL { ?subject dct:description ?description}
       OPTIONAL { ?subject dbo:thumbnail ?thumbnail .}
 
-      ${keywords.map((k, i) => `BIND(exists{?subject dbo:abstract ?d . ?d bif:contains "'${k}'"} as ?ab${i})`).join('\n')}
-      ${keywords.map((k, i) => `BIND(exists{?subject foaf:name ?name . ?name bif:contains "'${k}'"} as ?na${i})`).join('\n')}
+      ${keywords.map((k, i) => `BIND(exists{?subject dct:description ?de . ?de bif:contains "'${k}'"} as ?de${i})`).join('\n')}
+      ${keywords.map((k, i) => `BIND(exists{?subject dbo:abstract ?ab . ?ab bif:contains "'${k}'"} as ?ab${i})`).join('\n')}
+      ${keywords.map((k, i) => `BIND(exists{?subject foaf:name ?na . ?na bif:contains "'${k}'"} as ?na${i})`).join('\n')}
 
-      FILTER (LANG(?name)='en' && (${varAbstract.join('||')}))
+      FILTER (LANG(?name)='en')
     }
     ORDER BY DESC(?score)
     LIMIT 56`.replace(/\s\s+/g, ' ').trim();

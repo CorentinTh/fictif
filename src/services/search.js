a981544a51a08e4e3ff2862e548b0b55d5972ad1
CorentinTh <corentin.thomasset74@gmail.com>
2020-01-27 15:20:51 +0100

    feat: improved request

--- src/services/search.js
+++ src/services/search.js
@@ -1,32 +1,36 @@
 import { sparqlExecutor } from './sparql';
 
-const getRelatedCharacters = keywords => {
-  const variables = keywords.map((_, i) => `?t${i}`);
+const cleanQuery = query => query.toLowerCase().replace(/(["'\\])/g, '\\$1').replace(/\s\s+/g, ' ').trim().split(' ');
 
-  return sparqlExecutor(`
+const buildRequest = keywords => {
+  const varAbstract = keywords.map((_, i) => `?ab${i}`);
+
+  return `
     select distinct
       ?name
       ?thumbnail
       ?description
-      (${variables.join('+')} as ?score)
+      (${varAbstract.join('+')} as ?score)
     where {
 
       ?subject rdf:type <http://dbpedia.org/ontology/FictionalCharacter> .
       ?subject dbo:abstract ?d .
-
-      ${keywords.map((k, i) => `BIND(REGEX(?d, "${k}", "i") as ?t${i})`).join('\n')}
-      FILTER (LANG(?name)='en' && (${variables.join('||')}))
-
       ?subject rdfs:label ?name
       OPTIONAL { ?subject dct:description ?description}
       OPTIONAL { ?subject dbo:thumbnail ?thumbnail .}
+
+      ${keywords.map((k, i) => `BIND(REGEX(?d, "(^| )${k}", "i") as ?ab${i})`).join('\n')}
+      FILTER (LANG(?name)='en' && (${varAbstract.join('||')}))
     }
     ORDER BY DESC(?score)
-    LIMIT 50`);
+    LIMIT 56`.replace(/\s\s+/g, ' ').trim();
 };
 
 const getCharactersFromQuery = async (query) => {
-  const data = await getRelatedCharacters(query.toLowerCase().split(' '));
+  const keywords = cleanQuery(query);
+  const request = buildRequest(keywords);
+  const data = await sparqlExecutor(request);
+
   return data && data.length > 0 ? data.map(node => Object.entries(node).reduce((acc, [key, data]) => Object.assign(acc, { [key]: data.value.replace(/\\\\/g, '') }), {})) : [];
 };
 
